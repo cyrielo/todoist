4354a33f6bab04265c83e0414f7241c298cea970
Cyril Ologho <cyril.paul@andela.com>
2017-08-01 21:04:22 +0100

    add actions

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import { createTodo, viewAllTodos, viewCompleted, updateTodo, deleteTodo } from 
+'./TodoActions';
+
+export {
+	createTodo,
+	viewAllTodos,
+	viewCompleted,
+	updateTodo,
+	deleteTodo
+}
